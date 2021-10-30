ef8b67234645f33aaf71dd88c359815f1e4e61a8
unknown <shumonshihab62@gmail.com>
2021-10-30 17:35:17 +0600

    updated port

--- index.js
+++ index.js
@@ -5,16 +5,12 @@ const app = express();
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
-const port = 5000;
+const port = process.env.PORT;
 
 const { MongoClient } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bvkvy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-
-
 
 
 async function run() {
